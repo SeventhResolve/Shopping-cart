862dd7dabb83c77e547336fd77821a120ebfddc0
Hackbright Student <info@hackbrightacademy.com>
2016-01-20 18:19:39 -0800

    adding to cart works

--- shoppingsite.py
+++ shoppingsite.py
@@ -7,7 +7,7 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken.
 """
 
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -50,7 +50,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id(59)
+    melon = melons.get_by_id(melon_id)
     print melon
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -59,6 +59,7 @@ def show_melon(melon_id):
 @app.route("/cart")
 def shopping_cart():
     """Display content of shopping cart."""
+
 
     # TODO: Display the contents of the shopping cart.
 
@@ -82,13 +83,28 @@ def add_to_cart(id):
     page and display a confirmation message: 'Successfully added to cart'.
     """
 
+
+
+    # loop though list, if list is empty, stick melon into list
+    # else append melon to list
+    # session["melon_cart"] = 
+    # empty_list.append("asdf")
+    
+    if session.get('melon_cart', []) :
+        session['melon_cart'].append('asdf')
+        print "Went through if loop"
+
     # TODO: Finish shopping cart functionality
 
     # The logic here should be something like:
     #
     # - add the id of the melon they bought to the cart in the session
+    print "+++++++++++++++++++++++++++++"
+    print session['melon_cart']
+    # session['melon_id'] = 
 
-    return "Oops! This needs to be implemented!"
+
+    return "BLAH!!!!!!"
 
 
 @app.route("/login", methods=["GET"])
